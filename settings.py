b1fe3f76a516f7d1cca57cbe58900f73abc5213f
Kenn Knowles <kenn.knowles@gmail.com>
2013-06-20 18:15:12 -0400

    First draft of proper deps and tests

---
+++ settings.py
@@ -0,0 +1,59 @@
+# vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
+
+SECRET_KEY = 'this is not a secret key'
+
+INSTALLED_APPS = (
+    'couchlog',
+    'couchdbkit.ext.django',
+    'coverage',
+    'django.contrib.contenttypes',
+    'django.contrib.auth',
+)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'couchlog',
+    }
+}
+
+
+
+####### Couch Config ######
+COUCH_HTTPS = False # recommended production value is True if enabling https
+COUCH_SERVER_ROOT = '127.0.0.1:5984' #6984 for https couch
+COUCH_USERNAME = ''
+COUCH_PASSWORD = ''
+COUCH_DATABASE_NAME = 'couchlog'
+
+COUCH_DATABASE = 'http://127.0.0.1:5984/couchlog_test'
+
+COUCHDB_DATABASES = [ (app, 'http://127.0.0.1:5984/couchlog') for app in ['couch', 'couchlog'] ]
+
+TEST_RUNNER = 'couchdbkit.ext.django.testrunner.CouchDbKitTestSuiteRunner'
+
+####### # Email setup ########
+# Print emails to console so there is no danger of spamming, but you can still get registration URLs
+EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
+EMAIL_LOGIN = "nobody@example.com"
+EMAIL_PASSWORD = "******"
+EMAIL_SMTP_HOST = "smtp.example.com"
+EMAIL_SMTP_PORT = 587
+
+# Disable logging during testing
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'django.utils.log.NullHandler',
+        },
+    },
+    'loggers': {
+        '': {
+            'level': 'CRITICAL',
+            'handler': 'null',
+            'propagate': False,
+        }
+    }
+}
