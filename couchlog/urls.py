108937bb588c3ae3011827855c8d6a4f8b2a2a47
Nick Pellegrino <npellegrino@dimagi.com>
2016-09-13 10:11:47 +0530

    pass callable to url

--- couchlog/urls.py
+++ couchlog/urls.py
@@ -1,15 +1,24 @@
 from django.conf.urls import patterns, url
 
+from couchlog.views import (
+    dashboard,
+    email,
+    lucene_docs,
+    paging,
+    single,
+    update,
+)
+
 urlpatterns = patterns('',
-    url(r'^$', 'couchlog.views.dashboard', name='couchlog_home'),
-    url(r'^update/$', 'couchlog.views.update', name='couchlog_update'),
-    url(r'^email/$', 'couchlog.views.email', name='couchlog_email'),
-    url(r'^view/(?P<log_id>\w+)/$', 'couchlog.views.single', 
+    url(r'^$', dashboard, name='couchlog_home'),
+    url(r'^update/$', update, name='couchlog_update'),
+    url(r'^email/$', email, name='couchlog_email'),
+    url(r'^view/(?P<log_id>\w+)/$', single,
         {"display":"full"}, name='couchlog_single'),
-    url(r'^searchdocs/$', 'couchlog.views.lucene_docs', name='lucene_docs'),
+    url(r'^searchdocs/$', lucene_docs, name='lucene_docs'),
     
-    url(r'^ajax/paging/$', 'couchlog.views.paging', name='couchlog_paging'),
-    url(r'^ajax/single/(?P<log_id>\w+)/$', 'couchlog.views.single', 
+    url(r'^ajax/paging/$', paging, name='couchlog_paging'),
+    url(r'^ajax/single/(?P<log_id>\w+)/$', single,
         {"display":"ajax"}, name='couchlog_shortsingle'),
     
 )
